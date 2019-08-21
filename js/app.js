d26e1a0a6ed2ab24ef658e66e188fd82424220a4
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-08-21 15:28:51 +0300

    Update scroll helper visibility
    - Should only display as user scrolls from the top.
    
    [Tickets: #8]

--- js/app.js
+++ js/app.js
@@ -4,9 +4,12 @@ const mainSections = document.querySelectorAll('main section');
 // let lastId;
 // let cur = [];
 
-window.addEventListener('scroll', activeSectionSelector);
+window.addEventListener('scroll', debounce(highlightCurrentSection));
+window.addEventListener('scroll', debounce(hideOrShowScrollHelper));
 
-function activeSectionSelector (event) {
+
+
+function highlightCurrentSection () {
   let fromTop = window.scrollY;
   
   mainNavLinks.forEach(link => {
@@ -22,3 +25,32 @@ function activeSectionSelector (event) {
     }
   });
 }
+
+let scrollPos = 100;
+const scrollHelper = document.querySelector('.scroll-helper');
+
+function hideOrShowScrollHelper() {
+  let windowY = window.scrollY;
+
+  if(windowY < scrollPos) {
+    scrollHelper.classList.add('disappear_js');
+  } else {
+    scrollHelper.classList.remove('disappear_js');
+  }
+}
+
+
+function debounce(func, wait = 10, immediate = true) {
+  let timeout;
+  return function() {
+    let context = this, args = arguments;
+    let later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context,args);
+  };
+};
